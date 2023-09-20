299e91e05a55c0b29744e677e6bac1478b46e6cb
steezy <karadimos.giannis@gmail.com>
2023-09-20 21:34:37 +0300

    test3 fix 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -25,7 +25,7 @@ function ScrollToTop() {
 
 function App(): JSX.Element {
   return (
-    <Router basename="/ReactProjectM">
+    <Router>
       <Navbar />
       <ScrollToTop />
       <Routes>
